4ff3c75ac5372fcc80da9459c89c379a8d13b090
jonty Trombik <jat157@uclive.ac.nz>
2015-09-27 15:40:59 +1300

    -Added send/receieve strike methods
    -JavaDoc

--- assignment/final/ir_handler.c
+++ assignment/final/ir_handler.c
@@ -1,4 +1,8 @@
-
+/** @file   ir_handler.c
+    @author J. Trombik & J. Griffiths
+    @date   27 SEPT 2015
+    @brief IR handling and message manipulation
+*/
 
 #include "ir_handler.h"
 
@@ -12,8 +16,8 @@ static int8_t rcvChar;
 
 
 void ir_send_status(states status){
-  
-  ir_uart_putc_nocheck(status);
+
+  ir_uart_putc(status);
 
 }
 
@@ -38,5 +42,29 @@ states ir_get_status(void){
   }
   return NORESPONSE_S;
 
+}
+
+void ir_send_strike(tinygl_point_t pos) {
+  char msg = (pos.x << 4) | pos.y
+  ir_uart_putc(msg);
+
+}
+
+
+uint8_t ir_get_position(){
+  if (ir_uart_read_ready_p ())
+  {
+    char rcvdChar =  ir_uart_getc();
+    return rcvdChar;
+
+  }
+
+  return NO_POSITION;
+}
+
+tinygl_point_t ir_decode_strike(char c) {
+  uint8_t x = c & 0xf;
+  uint8_t y = c >> 4;
+  return tinygl_point(x, y)
 
 }
--- assignment/final/ir_handler.h
+++ assignment/final/ir_handler.h
@@ -1,7 +1,14 @@
+/** @file   ir_handler.c
+    @author J. Trombik & J. Griffiths
+    @date   27 SEPT 2015
+    @brief IR handler headers
+*/
+
 #ifndef IR_HANDLER_H
 #define IR_HANDLER_H
 
 #include "ir_uart.h"
+#include "tinygl.h"
 
 typedef enum {NORESPONSE_S, READY_S, ACKNOWLEDGED_S,  PLAYER_TWO_S} states;
 
@@ -9,4 +16,7 @@ void ir_send_status(states status);
 
 states ir_get_status(void);
 
+void ir_send_strike(tinygl_point_t pos);
+
+tinygl_point_t ir_decode_strike(char c);
 #endif
